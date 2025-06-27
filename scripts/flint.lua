baed37c4f59fa442f52de2b7c5b35b0a5c8e8520
KotIsOff <kot252steam@gmail.com>
2025-06-27 21:01:03 +0300

    not_crafting when???????????????777

--- scripts/flint.lua
+++ scripts/flint.lua
@@ -4,15 +4,15 @@ local not_crafting = "not_crafting";
 function on_use_on_block(x, y, z, pid, normal)
   if block.get(x, y, z) == block.index("base:wood") then
     if not pack.is_installed(not_crafting) then
-      ---@diagnostic disable-next-line: undefined-field
       local available = table.has(pack.get_available(), not_crafting)
-      local text = '"not_crafting" is not installed! ';
-      if available then text = text .. "You can add it to your world from contents." end
-      print(text);
+      local text = string.format('[#00FF00][NS] [#FFFF00]"%s" is not installed![#FFFFFF]', not_crafting);
+      if available then text = string.format("%s [#709900]But you can add it from contents.[#FFFFFF]", text) end
+      console.chat(text);
       return;
     end;
-    block.set(x, y, z, block.index(not_crafting .. ":primitive_crafting_table"))
-    inventory.consume_selected(pid, 1);
+    block.place(x, y, z, block.index(not_crafting .. ":primitive_crafting_table"), pid)
+    local inv, slot = player.get_inventory(pid)
+    inventory.decrement(inv, slot, 1)
     return true
   end
 end
