01d2ce2bd54eea1ada3bde1871d0eef5e22ccc07
KotIsOff <kot252steam@gmail.com>
2026-06-05 22:06:15 +0300

    feat: system enabled state

--- modules/shared/lib/system_instance.lua
+++ modules/shared/lib/system_instance.lua
@@ -5,13 +5,17 @@ return {
     ---@class ns.ecs.system
     ---@field name str
     local instance = {
-      name = name
+      name = name,
+      enabled = true
     }
 
     function instance:init() end;
 
+    ---@param bool bool
+    function instance:enable(bool) self.enabled = bool end;
+
     ---@return bool
-    function instance:should_update(id) return true end;
+    function instance:should_update(id) return self.enabled end;
 
     function instance:update(id, tps) end;
 
