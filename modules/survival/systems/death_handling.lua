37d40f53614411841d69d03d2b87b7e37a11c49f
KotIsOff <71443736+kotisoff@users.noreply.github.com>
2026-07-21 01:14:47 +0300

    fix: second cancel being nil

--- modules/survival/systems/death_handling.lua
+++ modules/survival/systems/death_handling.lua
@@ -14,8 +14,11 @@ events.on(resource("player_tick"), function(pid)
     local damage = variables.get_player_damage(pid);
 
     local canceled = events.emit("not_survival:before_death", pid, damage.amount, damage.type);
+    if type(canceled) == "nil" then
+      canceled = false;
+    end
 
-    print(string.format("ns: death canceled = %s"), canceled);
+    -- print(string.format("ns: death canceled = %s", canceled));
     if canceled then return end;
 
     document.reason.text = "Died " .. damage.type;
