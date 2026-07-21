c0bfbec9d346deea89cdb2277535ced4ee0701d5
KotIsOff <71443736+kotisoff@users.noreply.github.com>
2026-07-21 21:58:21 +0300

    refactor: sprint system

--- modules/server/systems/sprint_starvation.lua
+++ modules/server/systems/sprint_starvation.lua
@@ -1,27 +1,29 @@
-local mp               = require "shared/utils/not_utils".multiplayer.api.server
-local net_events       = require "shared/net/utils/net_events"
-local hunger           = require "shared/player/stats/hunger"
-local system_instance  = require "shared/lib/system_instance"
-local Counter          = require "shared/lib/Counter"
+local mp              = require "shared/utils/not_utils".multiplayer.api.server
+local net_events      = require "shared/net/utils/net_events"
+local hunger          = require "shared/player/stats/hunger"
+local system_instance = require "shared/lib/system_instance"
+local Counter         = require "shared/lib/Counter"
 
-local Sprinting_system = system_instance.new("ns.system.sprint_starvation")
+-- А это тут нужно чтобы работали приколы типа добавления кастомных полей и методов.
+---@class ns.ecs.system.server.sprinting:ns.ecs.system
+local SprintingSystem = system_instance.new("ns.system.sprint_starvation")
 
-function Sprinting_system:init()
+function SprintingSystem:init()
   ---@type bool[]
   self.sprinting = {};
 end
 
-function Sprinting_system:on_player_remove(id)
+function SprintingSystem:on_player_remove(id)
   Counter.get_or_create(id, self.name):destroy();
 end
 
 net_events.server.on(net_events.packets.player_sprinting, function(client, bytes)
   local status = unpack(mp.bson.deserialize(bytes))
 
-  Sprinting_system.sprinting[client.player.pid] = status
+  SprintingSystem.sprinting[client.player.pid] = status
 end)
 
-function Sprinting_system:update(pid, tps)
+function SprintingSystem:update(pid, tps)
   local sprint = Counter.get_or_create(pid, self.name);
 
   local is_sprinting = self.sprinting[pid] or false
@@ -36,4 +38,4 @@ function Sprinting_system:update(pid, tps)
   end
 end
 
-return Sprinting_system;
+return SprintingSystem;
