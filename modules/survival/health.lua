59edff3dfb2e62546f06ab8d213fb7147f2cf958
KotIsOff <71443736+kotisoff@users.noreply.github.com>
2026-07-23 22:33:25 +0300

    fix: damage in creative 2

--- modules/survival/health.lua
+++ modules/survival/health.lua
@@ -1,4 +1,5 @@
 local variables = require "player/variables";
+local death_manager = require "survival/death_manager"
 
 local health = {};
 
@@ -44,6 +45,8 @@ end
 ---@param do_knockback boolean | nil Knockback player or not.
 ---@param playsound string | boolean | nil Damage sound name or boolean.
 function health.damage(pid, damage, damage_type, source, do_knockback, playsound)
+  if death_manager.is_invulnerable(pid) then return "харам" end;
+
   source = source or { player.get_pos(pid) };
   playsound = playsound or true;
 
