177de6ee269f7c9363c8058a0f6213798dacc140
KotIsOff <71443736+kotisoff@users.noreply.github.com>
2026-09-03 01:03:12 +0300

    feat!: remove content (moved to ns_extras)

--- scripts/flint.lua
+++
@@ -1,6 +0,0 @@
-local not_crafting = "not_crafting";
-local event = "use_on_block";
-
-function on_use_on_block(x, y, z, pid, normal)
-  events.emit(string.format("%s:%s", not_crafting, event), item.index("not_survival:flint"), x, y, z, pid);
-end
